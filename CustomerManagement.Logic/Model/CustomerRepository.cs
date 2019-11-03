cdc5fdb16cfad5933b37faa4f35ec82082da105d
José Nery <josenerydev@gmail.com>
2019-11-02 23:02:38 -0300

    Repositories and EmailGateway.

---
+++ CustomerManagement.Logic/Model/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using CustomerManagement.Logic.Common;
+using CustomerManagement.Logic.Utils;
+
+using System.Linq;
+
+namespace CustomerManagement.Logic.Model
+{
+    public class CustomerRepository : Repository<Customer>
+    {
+        public CustomerRepository(UnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+
+        public Customer GetByName(string name)
+        {
+            return unitOfWork.Query<Customer>()
+                .SingleOrDefault(x => x.Name == name);
+        }
+    }
+}
---
+++ CustomerManagement.Logic/Model/IndustryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CSharpFunctionalExtensions;
+
+using CustomerManagement.Logic.Common;
+using CustomerManagement.Logic.Utils;
+
+using System.Linq;
+
+namespace CustomerManagement.Logic.Model
+{
+    public class IndustryRepository : Repository<Industry>
+    {
+        public IndustryRepository(UnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+
+        public Maybe<Industry> GetByName(string name)
+        {
+            return unitOfWork.Query<Industry>()
+                .SingleOrDefault(x => x.Name == name);
+        }
+    }
+}
