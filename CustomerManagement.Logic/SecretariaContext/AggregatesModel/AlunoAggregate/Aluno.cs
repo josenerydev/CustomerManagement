86af0046fb7d4210422fc071d03e24056683a698
josenerydev <josenerydev@gmail.com>
2019-11-07 16:05:29 -0300

    SecretariaContext.

---
+++ CustomerManagement.Logic/SecretariaContext/AggregatesModel/AlunoAggregate/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using CustomerManagement.Logic.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.Logic.SecretariaContext.AggregatesModel.AlunoAggregate
+{
+    public class Aluno : AggregateRoot
+    {
+        public string Foto { get; set; }
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+
+        // Dados Adicionais
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string EstadoCivil { get; set; }
+        // Casado, Divorciado, Solteiro
+
+        public string DataNascimento { get; set; }
+
+        public string NaturalDe { get; set; }
+        public string Nacionalidade { get; set; }
+
+        public string Carreira { get; set; }
+        public string Email { get; set; }
+        public string LinguaEstrangeira { get; set; }
+
+        public string ColegioAnterior { get; set; }
+        // Nome; Cidade; UF
+
+    }
+}
---
+++ CustomerManagement.Logic/SecretariaContext/AggregatesModel/AlunoAggregate/Responsavel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.Logic.SecretariaContext.AggregatesModel.AlunoAggregate
+{
+    public class Responsavel
+    {
+        public string Nome { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Profissão { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+
+        public bool ResponsavelFinanceiro { get; set; }
+        public bool ResponsavelPedagogico { get; set; }
+        public string GrauDeParentesco { get; set; }
+    }
+}
